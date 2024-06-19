92480c195cb0c64002ca092f91410ec3977eedf6
Lucas Abello <124079018+lexO-dat@users.noreply.github.com>
2024-06-19 15:32:57 -0400

    Update multiple_models.py

--- raspberry_app_lite/multiple_models.py
+++ raspberry_app_lite/multiple_models.py
@@ -3,29 +3,30 @@ import mediapipe as mp
 import numpy as np
 import tensorflow as tf
 
-# Initialize MediaPipe Hands.
+#Se inicializa mediapipe
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-# Cargando el modelo para determinar el grupo de letras
+#Carga el modelo general a tensorflow
 interpreter_groups = tf.lite.Interpreter(model_path="../Model/GENERAL_MODEL.tflite")
 interpreter_groups.allocate_tensors()
 input_details_groups = interpreter_groups.get_input_details()
 output_details_groups = interpreter_groups.get_output_details()
 
-#Setup for hand detection.
+#Setea los parametros de la deteccion de la mano
 hands = mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.7,
     min_tracking_confidence=0.7)
 
-#Video capture initialization.
+#Inicia la captura de vide (en la raspberry es la ip de la esp32-cam)
 cap = cv2.VideoCapture(0)
 
-#Etiquetas para los grupos de letras
+#asigna los grupos para el modelo general
 labels_groups = ["CO", "HG", "AST", "IJY"]
 
+#mapea las letras para usarlas con el modelo especifico
 specific_labels_dict = {
     "CO": ["c", "o"],
     "HG": ["h", "g"],
@@ -34,23 +35,25 @@ specific_labels_dict = {
 }
 
 while cap.isOpened():
+
+    #inicia la captura de imagenes
     success, image = cap.read()
     if not success:
         print("Ignoring empty camera frame.")
         continue
 
-    # Convert the image from BGR to RGB.
+    #Convierte la imagen capturada de BGR a RGB ya que opencv la captura en BGR
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
     results = hands.process(image)
 
-    # Prepare a white image for drawing.
+    #Se genra una imagen con el fondo de color blanco (255,255,255) para asi poner el esqueleto de la mano encima
     image_white = np.ones(image.shape, dtype=np.uint8) * 255
 
-    # Process each hand landmark.
+    #procesa la imagen y genera el esqueleto de distintos colores
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            # Draw hand landmarks.
+            #dibuja el esqueleto
             mp_drawing.draw_landmarks(
                 image_white,
                 hand_landmarks,
@@ -58,17 +61,17 @@ while cap.isOpened():
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
 
-            # Prepare the image for the model.
-            img_resize = cv2.resize(image_white, (224, 224))  # Resize if necessary (already 224x224)
-            input_data = np.expand_dims(img_resize.astype(np.float32) / 255.0, axis=0)  # Normalize the image
+            #prepara la imagen para el modelo
+            img_resize = cv2.resize(image_white, (224, 224))  #recortandola a 224x224
+            input_data = np.expand_dims(img_resize.astype(np.float32) / 255.0, axis=0)  #normaliza la imagen
             interpreter_groups.set_tensor(input_details_groups[0]['index'], input_data)
             interpreter_groups.invoke()
             output_data = interpreter_groups.get_tensor(output_details_groups[0]['index'])
-            group_index = np.argmax(output_data)
-            group_label = labels_groups[group_index]
+            group_index = np.argmax(output_data) #obtiene la prediccion viendo que prediccion tiene la confianza mas alta
+            group_label = labels_groups[group_index] #busca ese grupo en el labels para derivarlo al modelo especifico
             #print("Grupo detectado:", group_label)
             
-            
+            #se llama al modelo especifico
             path_to_specific_model = f"../Model/{group_label}.tflite"
             interpreter_specific = tf.lite.Interpreter(model_path=path_to_specific_model)
             interpreter_specific.allocate_tensors()
@@ -77,11 +80,13 @@ while cap.isOpened():
             interpreter_specific.set_tensor(input_details_specific[0]['index'], input_data)
             interpreter_specific.invoke()
             output_data_specific = interpreter_specific.get_tensor(output_details_specific[0]['index'])
+            #se extraen los labels conseguidos con el modelo general y se usa la prediccion mas alta
             letter_index = np.argmax(output_data_specific)
             specific_labels = specific_labels_dict[group_label]
+            #devuelve la letra detectada
             current_letter = specific_labels[letter_index]
 
-            print("Predicted Letter:", current_letter)
+            print("Letra predecida:", current_letter)
 
     # Display the result.
     cv2.imshow('MediaPipe Hands', image_white)
